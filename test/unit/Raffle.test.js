eb123d510fa666ea156ca8406f234e57d5547801
Priyam Soni <priyamsoni15@gmail.com>
2023-02-10 14:32:03 +0530

    Edits

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -2,24 +2,24 @@ const { assert } = require("chai")
 const { network, getNamedAccounts, deployments, ethers, log } = require("hardhat")
 const { devChains, networkConfig } = require("../../helper-hardhat-config")
 
-/*!devChains.includes(network.name)
+!devChains.includes(network.name)
     ? describe.skip
-    :*/ describe("Raffle", async () => {
-    let raffle, vrfCoordinatorV2Mock
-    const chainId = network.config.chainId
+    : describe("Raffle", async () => {
+          let raffle, vrfCoordinatorV2Mock
+          const chainId = network.config.chainId
 
-    beforEach(async () => {
-        const deployer = (await getNamedAccounts()).deployer
-        await deployments.fixture(["all"])
-        raffle = await ethers.getContract("Raffle", deployer)
-        log("Pass")
-        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
-    })
+          beforEach(async () => {
+              const deployer = (await getNamedAccounts()).deployer
+              await deployments.fixture(["all"])
+              raffle = await ethers.getContract("Raffle", deployer)
+              log("Pass")
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+          })
 
-    it("initializes the Raffle Correctly", async () => {
-        const raffleState = await raffle.getRaffleState()
-        const interval = await raffle.getInterval()
-        assert.equal(raffleState.toString(), "0")
-        assert.equal(interval.toString(), networkConfig[chainId]["interval"])
-    })
-})
+          it("initializes the Raffle Correctly", async () => {
+              const raffleState = await raffle.getRaffleState()
+              const interval = await raffle.getInterval()
+              assert.equal(raffleState.toString(), "0")
+              assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+          })
+      })
