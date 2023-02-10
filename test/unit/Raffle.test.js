1669582965d76721043de522a049f86338e16a4d
Priyam Soni <priyamsoni15@gmail.com>
2023-02-10 14:30:35 +0530

    Initial Commit

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,26 +1,25 @@
 const { assert } = require("chai")
-const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
+const { network, getNamedAccounts, deployments, ethers, log } = require("hardhat")
 const { devChains, networkConfig } = require("../../helper-hardhat-config")
 
-!devChains.includes(network.name)
+/*!devChains.includes(network.name)
     ? describe.skip
-    : describe("Raffle", async () => {
-        let raffle, vrfCoordinatorV2Mock
-        const chainId = network.config.chainId
+    :*/ describe("Raffle", async () => {
+    let raffle, vrfCoordinatorV2Mock
+    const chainId = network.config.chainId
 
-        beforEach(async () => {
-            const { deployer } = await getNamedAccounts()
-            await deployments.fixture(["all"])
-            raffle = await ethers.getContract("Raffle", deployer)
-            vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
-        })
-        describe("Constructor", async () => {
-            it("Initializes the Raffle Correctly", async () => {
-                const raffleState = await raffle.getRaffleState()
-                const interval = await raffle.getInterval()
-                assert.equals(raffleState.toString(), "0")
-                assert.equals(interval.toString(), networkConfig[chainId]["interval"])
-            })
-        })
-
+    beforEach(async () => {
+        const deployer = (await getNamedAccounts()).deployer
+        await deployments.fixture(["all"])
+        raffle = await ethers.getContract("Raffle", deployer)
+        log("Pass")
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
     })
+
+    it("initializes the Raffle Correctly", async () => {
+        const raffleState = await raffle.getRaffleState()
+        const interval = await raffle.getInterval()
+        assert.equal(raffleState.toString(), "0")
+        assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+    })
+})
